65af19936619c46c73a46782ad191020cc9fcfad
sarahdolan1 <sarahdolan1969@hotmail.com>
2021-10-28 16:33:29 +0100

    Dag commit

--- LCAJava/LCA.java
+++ LCAJava/LCA.java
@@ -7,10 +7,12 @@ import java.util.List;
 class Node {
 	int data;
 	Node left, right;
-
+	ArrayList<Node> ancestors;
+	
 	Node(int value) {
 		data = value;
 		left = right = null;
+		ancestors = null;
 	}
 }
 
@@ -23,9 +25,9 @@ public class LCA {
 	int functionLCA(int nodeOne, int nodeTwo) {
 		nodeOnePath.clear();
 		nodeTwoPath.clear();
-		return functionLCA(root, nodeOne, nodeTwo);
+		return functionLCADAG(root, nodeOne, nodeTwo);
 	}
-
+/*
 	private int functionLCA(Node root, int nodeOne, int nodeTwo) {
 		if (!findPath(root, nodeOne, nodeOnePath) || !findPath(root, nodeTwo, nodeTwoPath)) {
 			if(nodeOnePath.size() == 0 && nodeTwoPath.size() == 0) {
@@ -51,7 +53,37 @@ public class LCA {
 		}
 		return nodeOnePath.get(index - 1);
 	}
-
+*/
+	private int functionLCADAG(Node root, int nodeOne, int nodeTwo) {
+		if(!findPath(root, nodeOne, nodeOnePath)|| !findPath(root, nodeTwo, nodeTwoPath)) {
+			return -1;
+		}
+		int i;
+		for(i=0; i < nodeOnePath.size() && i < nodeTwoPath.size(); i++) {
+			if(!nodeOnePath.get(i).equals(nodeTwoPath.get(i))) {
+				break;
+			}
+		}
+		return nodeOnePath.get(i-1);
+		
+	}
+	
+	protected int functionLCADAG2(Node root, Node nodeOne, Node nodeTwo) {
+		if(nodeOne !=null && nodeTwo != null) {
+			if(nodeOne.ancestors !=null && nodeTwo.ancestors != null) {
+				for(int i = 0; i <nodeTwo.ancestors.size();i++) {
+					for (int j = 0;j<nodeOne.ancestors.size();j++) {
+						if(nodeTwo.ancestors.get(i) == nodeOne.ancestors.get(j)) {
+							return nodeTwo.ancestors.get(i).data;
+						}
+					}
+				}
+			}else {
+				return -1;
+			}
+		}
+		return -1;
+	}
 	private boolean findPath(Node root, int num, List<Integer> path) {
 		if (root == null) {
 			return false;	
@@ -70,4 +102,28 @@ public class LCA {
 			return false;
 		}
 	}
+	public void add2Graph(Node nodeOne) {
+		nodeOne.ancestors = new ArrayList<Node>();
+		nodeOne.ancestors.add(nodeOne);
+		
+	}
+	public void addAncestors(Node nodeOne, Node nodeTwo) {
+		int i;
+		for(i=0; i<nodeOne.ancestors.size();i++) {
+			if(!nodeTwo.ancestors.contains(nodeOne.ancestors.get(i))) {
+				nodeTwo.ancestors.add(nodeOne.ancestors.get(i));
+			}
+			
+		}
+	}
+	public void addAncestorsnode2(Node nodeOne, Node nodeTwo, int place) {
+		int i;
+		for(i=0; i<nodeOne.ancestors.size();i++) {
+			if(!nodeTwo.ancestors.contains(nodeOne.ancestors.get(i))) {
+				nodeTwo.ancestors.add(place, nodeOne.ancestors.get(i));
+			}
+			
+		}
+	}
+	
 }
